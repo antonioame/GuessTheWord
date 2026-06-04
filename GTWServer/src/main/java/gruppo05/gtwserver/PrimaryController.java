f42baaa6ee9ec75065f594231d6cf217b9c44b59
francesco-vecchione <f.vecchione17@studenti.unisa.it>
2026-06-04 16:59:25 +0200

    Repo Clean State

---
+++ GTWServer/src/main/java/gruppo05/gtwserver/PrimaryController.java
@@ -0,0 +1,12 @@
+package gruppo05.gtwserver;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}

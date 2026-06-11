37e4f1c9f6220b03194d235abbf0dc76ed3e85b2
francesco-vecchione <f.vecchione17@studenti.unisa.it>
2026-06-11 14:17:53 +0200

    Feat: Aggiunti alert nel caso di fallimento o successo della registrazione di un nuovo admin

--- GTWServer/src/main/java/gruppo05/gtwserver/controller/ServerSignupManager.java
+++ GTWServer/src/main/java/gruppo05/gtwserver/controller/ServerSignupManager.java
@@ -4,7 +4,9 @@ import gruppo05.gtwserver.db.AdminDAO;
 import gruppo05.gtwserver.db.ConcreteAdminDAO;
 import gruppo05.gtwserver.model.Admin;
 import gruppo05.gtwshared.controller.SignupManager;
+import java.util.Optional;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
 
 public class ServerSignupManager implements SignupManager {
 
@@ -18,7 +20,15 @@ public class ServerSignupManager implements SignupManager {
     public void registerInfo(String username, String password) {
         AdminDAO dao = new ConcreteAdminDAO();
         
+        if(dao.selectById(Optional.of(username)).isPresent()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "La registrazione non è andata a buon fine");
+            alert.showAndWait();
+            return;
+        } 
+        
         dao.insert(new Admin(username, password));
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Il nuovo admin è stato registrato correttamente");
+        alert.showAndWait();
         
         if (onSuccessCallback != null) {
             Platform.runLater(onSuccessCallback);
